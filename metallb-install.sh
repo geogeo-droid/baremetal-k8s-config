f563f556057afedc3120031c05fbd5a896fd5e59
geogeo-droid <79016201+geogeo-droid@users.noreply.github.com>
2021-12-19 19:18:44 +0900

    remove: removeed metallb-install.yaml

--- metallb-install.sh
+++ metallb-install.sh
@@ -2,8 +2,17 @@
 # kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.4/manifests/metallb.yaml
 # kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
 
+# mac
+# ./docker-tuntap-osx/sbin/docker_tap_install.sh
+# ./docker-tuntap-osx/sbin/docker_tap_up.sh
+# sudo route -v add -net 172.18.0.1 -netmask 255.255.0.0 10.0.75.2
 
 helm install metallb metallb/metallb -f metallb-values.yaml
-./docker-tuntap-osx/sbin/docker_tap_install.sh
-./docker-tuntap-osx/sbin/docker_tap_up.sh
-sudo route -v add -net 172.18.0.1 -netmask 255.255.0.0 10.0.75.2
+kubectl  create secret generic docker-config --from-file=.dockerconfigjson=config.json --type=kubernetes.io/dockerconfigjson
+kubectl apply -f calico.yaml-3.20
+calicoctl create -f calico-bgp.yaml
+calicoctl create -f calico-peer.yaml
+
+helmfile sync
+
+kubectl apply -f metallb-test-ingress.yaml
